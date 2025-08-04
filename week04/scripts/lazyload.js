41457fbd5c214aae284c3d8a403acf2245988427
WizardAaron <ethanlong547@gmail.com>
2025-08-04 14:24:07 -0700

    prepare-lazyload activity complete

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,47 @@
+// select the DOM elements for output
+const full = document.querySelector("#full");
+const worldfull = document.querySelector("#world-full");
+const short = document.querySelector("#short");
+const medium = document.querySelector("#medium");
+const month = document.querySelector("#month");
+const day = document.querySelector("#day");
+const dayofweek = document.querySelector("#dayofweek");
+
+const lastModified = document.querySelector("#short");
+lastModified.innerHTML = document.lastModified;
+const year = document.querySelector("#year");
+console.log("Found year element:", year); // For debugging
+year.textContent = new Date().getFullYear();
+
+// use the date object
+const today = new Date();
+
+full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "full"
+	}
+).format(today)}</span>`;
+worldfull.innerHTML = `UK: <span class="highlight">${new Intl.DateTimeFormat(
+	"en-UK",
+	{
+		dateStyle: "full"
+	}
+).format(today)}</span>`;
+short.innerHTML = `Short: <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "short"
+	}
+).format(today)}</span>`;
+medium.innerHTML = `Medium: <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "medium"
+	}
+).format(today)}</span>`;
+
+year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+month.innerHTML = `getMonth(): <span class="highlight">${today.getMonth()}</span>`;
+day.innerHTML = `getDate(): <span class="highlight">${today.getDate()}</span>`;
+dayofweek.innerHTML = `getDay(): <span class="highlight">${today.getDay()}</span>`;
